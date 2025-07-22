c936e58516ee0be4ba91f68ee4a279dd4af7acfe
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-22 22:45:48 +0100

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,7 @@
+-- Task:
+-- Write a SELECT statement to get the names and Math scores of all students in grade 11
+-- The students table has the attributes: student_id, name, grade_level
+-- The results table has the attributes: student_id (FK), subject, score
+-- Order results by score from highest to lowest
+
+-- Your query here:
