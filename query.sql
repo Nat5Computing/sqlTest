d4dc1eba1bc808b69c418598dd03fe4eb4f89c36
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-24 22:20:58 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,7 +1,14 @@
 -- Task:
 -- Write a SELECT statement to get the names and Math scores of all students in grade 11
--- The students table has the attributes: student_id, name, grade_level
--- The results table has the attributes: student_id (FK), subject, score
+-- The students entity has the attributes: student_id, name, grade_level
+-- The results entity has the attributes: student_id (FK), subject, score
 -- Order results by score from highest to lowest
 
 -- Your query here:
+
+
+
+
+-- To run your query, in the terminal type: chmod +x test_query.sh && ./test_query.sh
+-- Press the enter key.
+-- No terminal showing? On the top left drop down menu go to View > Terminal
